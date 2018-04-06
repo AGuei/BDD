2a55e7308d66cd4cee107a190d6ddab1aba1d770
AGuei <AGuei@users.noreply.github.com>
2018-04-06 23:06:17 +0800

    增加註解

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ var gulp = require('gulp'),               // 載入 gulp
     gulpLivereload = require('gulp-livereload');  // 載入 gulp-livereload
 
 gulp.task('watch', function () {
-    gulpLivereload.listen();
-    gulp.watch('*.html', ['html']);    
+    gulpLivereload.listen();  // 在執行 gulp watch 指令後，順便啟動 livereload 伺服器
+    gulp.watch('*.html', ['html']);    // 在 html 檔案有異動時，執行名為 html 的任務(task)
 });
 
-gulp.task('html', function () {
+gulp.task('html', function () {  // 名為 html 的任務(task)
     gulp.src('*.html')
         .pipe(gulpLivereload());
 })
