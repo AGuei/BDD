b570015c41534aa1394e47dea8a78e805aa8a8fd
AGuei <AGuei@users.noreply.github.com>
2018-04-06 18:59:08 +0800

    第一份可以運作livereload的gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,10 @@ var gulp = require('gulp'),               // 載入 gulp
 
 gulp.task('watch', function () {
     gulpLivereload.listen();
-    gulp.watch('html', ['html']);    
+    gulp.watch('*.html', ['html']);    
 });
+
+gulp.task('html', function () {
+    gulp.src('*.html')
+        .pipe(gulpLivereload());
+})
