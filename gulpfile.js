4e3560c9792ec3d53d2e09eee7aa22c7329b16cc
AGuei <AGuei@users.noreply.github.com>
2018-04-06 18:39:55 +0800

    新增gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+var gulp = require('gulp'),               // 載入 gulp
+    gulpLivereload = require('gulp-livereload');  // 載入 gulp-livereload
+
+gulp.task('watch', function () {
+    gulpLivereload.listen();
+    gulp.watch('html', ['html']);    
+});
