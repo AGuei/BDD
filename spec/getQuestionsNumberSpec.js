fe5c4108f23093745f8fee686938e9b54f095504
AGuei <AGuei@users.noreply.github.com>
2018-04-08 02:17:12 +0800

    讓測試案例失敗

--- spec/getQuestionsNumberSpec.js
+++ spec/getQuestionsNumberSpec.js
@@ -2,10 +2,10 @@ var module = require('../getQuestionsNumber.js');
 
 describe('find questions numbers', () => {
     it('should find all questions numbers', () => {
-        let questions = '1. xxxxxx\n2. aaaasadf\n3. 444asdjfj';
+        let questions = '1. xxxxxx\n2. aaaasadf\n3. 444asdjfj\n4. asdfasdf';
         let questionsRegex = /^\d{1,3}\.[\s\S]+?/gum;
 
-        expect(module.getQuestionsNumbers(questions).length).toEqual(3, 'length cannot match');
-        expect(module.getQuestionsNumbers(questions)).toEqual(['1. ', '2. ', '3. '], "Numbers can't match");
+        expect(module.getQuestionsNumbers(questions).length).toEqual(3);
+        expect(module.getQuestionsNumbers(questions)).toEqual(['1. ', '2. ', '3. ']);
     });
 });
