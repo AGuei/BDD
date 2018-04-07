31b9d0cb9cde9739dc1e66b18cb8ab5d6e712257
AGuei <AGuei@users.noreply.github.com>
2018-04-08 02:08:38 +0800

    合併衝突時忘了存檔

--- getQuestionsNumber.js
+++ getQuestionsNumber.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 module.exports = {
     getQuestionsNumbers: function (questions) {
         let questionsNumberRegexp = /^\d{1,3}\.[\s\S]+?/gum;
         return questions.match(questionsNumberRegexp);
     }
 };
-=======
-export function getQuestionsNumbers(quesions) {
-    let questionsNumberRegexp = /^\d{1,3}\.[\s\S]+?/gum;
-    return quesions.match(questionsNumberRegexp);
-}
->>>>>>> 4054e0642da1c46401dc2b3f3113c5487acc694a
