4c20184c3e768b581bd4ca3e8006e9e3dbeffb29
AGuei <AGuei@users.noreply.github.com>
2018-04-07 23:48:55 +0800

    找出題號的測試案例

---
+++ spec/findQuesionsNumberSpec.js
@@ -0,0 +1,9 @@
+describe('find quesions numbers', () => {
+    it('should find all quesions numbers', () => {
+        let quesions = '1. xxxxxx\n2. aaaasadf\n3. 444asdjfj';
+        let quesionsRegex = /^\d{1,3}\.[\s\S]+?/gum;
+
+        expect(getQuestionsNumbers(quesions).length).toEqual(3, 'length cannot match');
+        expect(getQuestionsNumbers(quesions)).toEqual(['1. ', '2. ', '3. '], "Numbers can't match");
+    });
+});
