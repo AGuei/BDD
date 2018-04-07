d2db1d9609735f618fda65b1230c3529bef6c569
AGuei <AGuei@users.noreply.github.com>
2018-04-08 01:05:41 +0800

    通過測試的找題號程式碼(ES6模組寫法)

--- getQuesionsNumber.js
+++ getQuesionsNumber.js
@@ -1,6 +1,4 @@
-module.exports = {
-    getQuestionsNumbers: function (quesions) {
-        let quesionsNumberRegexp = /^\d{1,3}\.[\s\S]+?/gum;
-        return quesions.match(quesionsNumberRegexp);
-    }
-};
+export function getQuestionsNumbers(quesions) {
+    let quesionsNumberRegexp = /^\d{1,3}\.[\s\S]+?/gum;
+    return quesions.match(quesionsNumberRegexp);
+}
