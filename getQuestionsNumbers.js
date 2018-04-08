6ddf74995d49a6225207bd8b2f267ac8e92af126
AGuei <AGuei@users.noreply.github.com>
2018-04-08 16:50:35 +0800

    將測試案例與待測程式碼改成 UMD 寫法以同時支援 Nodejs 與瀏覽器

--- getQuestionsNumbers.js
+++ getQuestionsNumbers.js
@@ -1,8 +1,21 @@
+/* eslint-env jasmine, browser, webextensions, amd */
 /* eslint-disable no-unused-vars */
 
-function getQuestionsNumbers (questions) {
-  let questionsNumberRegexp = /^\d{1,3}\.[\s\S]+?/gum;
-  return questions.match(questionsNumberRegexp);
-}
-
-// module.exports = getQuestionsNumbers;
+(function (root, factory) {
+  if (typeof module === 'object' && module.exports) {
+    // Node/CommonJS
+    module.exports = factory();
+  } else if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(factory);
+  } else {
+    // Browser globals
+    root.getQuestionsNumbers = factory();
+  }
+}(this, function factory () {
+  function getQuestionsNumbers (questions) {
+    let questionsNumberRegexp = /^\d{1,3}\.[\s\S]+?/gum;
+    return questions.match(questionsNumberRegexp);
+  }
+  return getQuestionsNumbers;
+}));
--- spec/getQuestionsNumbersSpec.js
+++ spec/getQuestionsNumbersSpec.js
@@ -1,14 +1,30 @@
-/* eslint-env jasmine, browser, webextensions */
-/* global getQuestionsNumbers */
+/* eslint-env jasmine, browser, webextensions, amd */
 
 // var getQuestionsNumbers = require('../getQuestionsNumbers.js');
 
-describe('find questions numbers', () => {
-  it('should find all questions numbers', () => {
-    let questions = '1. xxxxxx\n2. aaaasadf\n3. 444asdjfj';
-    // let questionsRegex = /^\d{1,3}\.[\s\S]+?/gum;
+(function (root, factory) {
+  if (typeof module === 'object' && module.exports) {
+    // Node/CommonJS
+    factory(
+      require('../getQuestionsNumbers')
+    );
+  } else if (typeof define === 'function' && define.amd) {
+    // AMD
+    define([
+      '../getQuestionsNumbers'
+    ], factory);
+  } else {
+    // Browser globals
+    factory(root.getQuestionsNumbers);
+  }
+}(this, function factory (getQuestionsNumbers) {
+  describe('find questions numbers', () => {
+    it('should find all questions numbers', () => {
+      let questions = '1. xxxxxx\n2. aaaasadf\n3. 444asdjfj';
+      // let questionsRegex = /^\d{1,3}\.[\s\S]+?/gum;
 
-    expect(getQuestionsNumbers(questions).length).toEqual(3);
-    expect(getQuestionsNumbers(questions)).toEqual(['1. ', '2. ', '3. ']);
+      expect(getQuestionsNumbers(questions).length).toEqual(3);
+      expect(getQuestionsNumbers(questions)).toEqual(['1. ', '2. ', '3. ']);
+    });
   });
-});
+}));
