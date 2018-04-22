e58a5bc43d527e3458ede2838bac700aad2c74f1
AGuei <AGuei@users.noreply.github.com>
2018-04-22 14:19:07 +0800

    新增移除換行符號及空白的測試案例

--- spec/getRegexpResultArraySpec.js
+++ spec/getRegexpResultArraySpec.js
@@ -16,17 +16,26 @@
     factory(root.getRegexpResultArray);
   }
 }(this, function factory (getRegexpResultArray) {
-  let questions = '1. xxxxxx\n2. aaaasadf\n3. 444asdjfj';
-  describe('Accept pattern from user\'s choice for finding questions numbers', () => {
-    it('should find all questions numbers if regexp do not include capture group', () => {
-      let getQuestionsNumbersPattern = '^\\d{1,3}\\.[\\s\\S]+?/gmu';
-      let result = getRegexpResultArray(questions, getQuestionsNumbersPattern);
-      expect(result).toEqual(['1. ', '2. ', '3. ']);
+  describe('Accept pattern from user\'s choice for finding somethins', () => {
+    let questions = '1. xxxxxx (A)\n2. aaa\nasadf (A)\n3. 444asdjfj (A)';
+    describe('Accept pattern from user\'s choice for finding questions numbers', () => {
+      it('should find all questions numbers if regexp do not include capture group', () => {
+        let getQuestionsNumbersPattern = '^\\d{1,3}\\.[\\s\\S]+?/gmu';
+        let result = getRegexpResultArray(questions, getQuestionsNumbersPattern);
+        expect(result).toEqual(['1. ', '2. ', '3. ']);
+      });
+      it('should find all questions numbers if regexp includes capture group', () => {
+        let getQuestionsNumbersPattern = '^(\\d{1,3}\\.)[\\s\\S]+?/gmu';
+        let result = getRegexpResultArray(questions, getQuestionsNumbersPattern);
+        expect(result).toEqual(['1.', '2.', '3.']);
+      });
     });
-    it('should find all questions numbers if regexp includes capture group', () => {
-      let getQuestionsNumbersPattern = '^(\\d{1,3}\\.)[\\s\\S]+?/gmu';
-      let result = getRegexpResultArray(questions, getQuestionsNumbersPattern);
-      expect(result).toEqual(['1.', '2.', '3.']);
+    describe('Accept pattern from user\'s choice for finding questions', () => {
+      it('should remove new line symbol and space at front and end', () => {
+        let getQuestionsPattern = '^\\d{1,3}\\.\\s{0,3}([\\s\\S]+?)(?=\\(A\\))/gmu';
+        let result = getRegexpResultArray(questions, getQuestionsPattern);
+        expect(result).toEqual(['xxxxxx', 'aaaasadf', '444asdjfj']);
+      });
     });
   });
 }));
